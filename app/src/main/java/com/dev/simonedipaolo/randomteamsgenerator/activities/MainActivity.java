db8207f85a909803ef486628e7990a9fab3b3f79
Simone Di Paolo <s.dipaolo90@gmail.com>
2023-01-31 23:23:56 +0100

    Feature - Added a working toolbar (topbar). Replaced the add button with a + in material design. Fixed the go back with entire names list from the generate teams fragment to the names list fragment. Other smaller fixes done.

--- app/src/main/java/com/dev/simonedipaolo/randomteamsgenerator/activities/MainActivity.java
+++ app/src/main/java/com/dev/simonedipaolo/randomteamsgenerator/activities/MainActivity.java
@@ -1,18 +1,27 @@
 package com.dev.simonedipaolo.randomteamsgenerator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.dev.simonedipaolo.randomteamsgenerator.R;
 import com.dev.simonedipaolo.randomteamsgenerator.fragments.MainFragment;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private MaterialToolbar materialToolbar;
+
+    private CoordinatorLayout coordinatorLayout;
+    private BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         materialToolbar = findViewById(R.id.materialToolbar);
+        materialToolbar.setTitle(StringUtils.EMPTY);
         setSupportActionBar(materialToolbar);
+
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+        coordinatorLayout.setVisibility(View.INVISIBLE);
+        bottomAppBar = findViewById(R.id.bottomAppBar);
 
     }
 
